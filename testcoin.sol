5a145906c625db5fbd24254b0b55314327615c26
Aliaksandr Bazhanau <a.bazhanau@adform.com>
2017-01-17 20:41:54 +0300

    approve function fixed

--- testcoin.sol
+++ testcoin.sol
@@ -60,7 +60,6 @@ contract token {
     function approve(address _spender, uint256 _value)
         returns (bool success) {
         allowance[msg.sender][_spender] = _value;
-        tokenRecipient spender = tokenRecipient(_spender);
         return true;
     }
 
