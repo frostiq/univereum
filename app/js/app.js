cc3c34ccbe82e1aad43124798d72459697b8d821
Aliaksandr Bazhanau <bazhanau@outlook.com>
2017-03-26 23:35:45 +0300

    migration to React.js

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,14 @@
+//import React from 'react';
+//import ReactDOM from 'react-dom';
 
 $(document).ready(function () {
   console.log("deployed at: " + Unitoken.address)
-  Unitoken.symbol().then(function (symbol) {
-    $("#tests").append("<br>test 1: " + symbol);
-  });
 });
+
+Unitoken.symbol().then(function (symbol) {
+  ReactDOM.render(
+    <h4>test 12: {symbol}</h4>,
+    document.getElementById("tests")
+  )
+})
 
